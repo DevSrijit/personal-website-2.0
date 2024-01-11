b177ce343e00f3adc858bfa951bd024e4aa24e2e
devsrijit <srijitghosh7331@gmail.com>
2024-01-11 22:50:33 +0530

    removed wrong metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import { Metadata } from 'next'
- 
-export const Metadata = {
-  title: 'Srijit Ghosh',
-  description: 'Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.',
-}
 
 const App = ({ Component, pageProps }) => {
   return (
