1b6d58d90ee003f3c16c799dabe0ba770d71885e
devsrijit <srijitghosh7331@gmail.com>
2024-01-11 23:40:12 +0530

    NextSeo configuration added

--- next.config.js
+++ next.config.js
@@ -8,6 +8,32 @@ const nextConfig = {
     author: "Srijit Ghosh",
     siteUrl: "https://srijit.life",
   },
+  nextSeo: {
+    title: "Srijit's Personal Website",
+    description:
+      "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
+    openGraph: {
+      url: "https://srijit.life",
+      images: [
+        {
+          url: "https://cloud-4tgxbh5wm-hack-club-bot.vercel.app/0frontend_dev.jpeg",
+          width: 630,
+          height: 630,
+          alt: "Image description",
+        },
+      ],
+      site_name: "Srijit's Personal Website",
+    },
+    twitter: {
+      handle: "@CheeseBlock011",
+      cardType: "summary_large_image",
+      title: "Srijit's Personal Website",
+      description:
+        "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
+      image:
+        "https://cloud-4tgxbh5wm-hack-club-bot.vercel.app/0frontend_dev.jpeg",
+    },
+  },
   head: {
     // Add custom meta tags
     meta: [
@@ -27,7 +53,8 @@ const nextConfig = {
       type: "website",
       url: "https://srijit.life",
       title: "Srijit's Personal Website",
-      description: "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
+      description:
+        "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
       images: [
         {
           url: "https://cloud-4tgxbh5wm-hack-club-bot.vercel.app/0frontend_dev.jpeg",
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { NextSeo } from "next-seo";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider
-      defaultTheme="system"
-    >
+    <ThemeProvider defaultTheme="system">
+      <NextSeo
+        openGraph={{
+          type: "website",
+          url: "https://srijit.life",
+          title: "Srijit's Personal Website",
+          description: "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
+          images: [
+            {
+              url: "https://cloud-4tgxbh5wm-hack-club-bot.vercel.app/0frontend_dev.jpeg",
+              width: 630,
+              height: 630,
+              alt: "Image description",
+            },
+          ],
+          site_name: "Srijit's Personal Website",
+        }}
+        // For Twitter
+        twitter={{
+          handle: "@CheeseBlock011",
+          cardType: "summary_large_image",
+          title: "Srijit's Personal Website",
+          description: "Srijit Ghosh is a 16yo frontend developer and technologist. This is his personal website.",
+          image: "https://cloud-4tgxbh5wm-hack-club-bot.vercel.app/0frontend_dev.jpeg",
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
