4780543929df2b2c06062c795c1640b030d740d0
devsrijit <srijitghosh7331@gmail.com>
2024-01-12 12:42:48 +0530

    Added SimpleAnalytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "queue.simpleanalyticscdn.com", "cloud-4tgxbh5wm-hack-club-bot.vercel.app"],
+  }
 };
 
 module.exports = nextConfig;
