0329a015026b3aa0600f77a586b8a007ea202bdd
devsrijit <srijitghosh7331@gmail.com>
2024-02-02 00:58:38 +0530

    Bug fix: Added tracking image domain link to next config file.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "queue.simpleanalyticscdn.com", "cloud-ppyu19102-hack-club-bot.vercel.app", "cloud-a8i9f763o-hack-club-bot.vercel.app", "cloud-a7uy4sxh9-hack-club-bot.vercel.app", "cloud-5dcd65ay2-hack-club-bot.vercel.app", "cloud-3etw3azne-hack-club-bot.vercel.app", "cloud-jeapr8koy-hack-club-bot.vercel.app"],
+    domains: ["images.unsplash.com", "queue.simpleanalyticscdn.com", "cloud-ppyu19102-hack-club-bot.vercel.app", "cloud-a8i9f763o-hack-club-bot.vercel.app", "cloud-a7uy4sxh9-hack-club-bot.vercel.app", "cloud-5dcd65ay2-hack-club-bot.vercel.app", "cloud-3etw3azne-hack-club-bot.vercel.app", "cloud-jeapr8koy-hack-club-bot.vercel.app", "sa.srijit.life"],
   }
 };
 
