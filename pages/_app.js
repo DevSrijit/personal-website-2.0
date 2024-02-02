1f317229ab87ad08ec395e2908d4e3130d161ced
devsrijit <srijitghosh7331@gmail.com>
2024-02-02 15:04:45 +0530

    Bug fix: Giving next/script id to fix nextjs errors.

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,12 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Script
+        id="gtag-script"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
+        id="gtag-config"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
