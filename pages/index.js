e936cac1a794fa917396827998b35dda28f95a93
Srijit <89468732+DevSrijit@users.noreply.github.com>
2024-02-02 17:14:04 +0530

    Revert title to maintain SEO keyword.

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
       <div className={`relative ${data.showCursor && "cursor-none"}`}>
         {data.showCursor && <Cursor />}
         <Head>
-          <title>Srijit Ghosh</title>
+          <title>Srijit&apos;s Personal Website</title>
         </Head>
 
         <div className="gradient-circle"></div>
