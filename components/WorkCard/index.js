8672a2fdddc4102ae8338f087a452767a8187bd4
devsrijit <srijitghosh7331@gmail.com>
2024-02-06 00:39:23 +0530

    Revert

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -7,13 +7,16 @@ const WorkCard = ({ img, name, description, onClick }) => {
       onClick={onClick}
     >
       <div
-        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-32 mob:h-auto"
+        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
+        style={{ height: "400px" }}
       >
-        <img
+        <Image
           alt={name}
           className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
           src={img}
-          fetchPriority="high"
+          priority={true}
+          fill={true}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
       <h1 className="mt-5 text-3xl font-medium">
