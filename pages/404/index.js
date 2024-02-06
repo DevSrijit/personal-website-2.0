505c0a0fdff9650e8c9e9226b9793896f08797e1
devsrijit <srijitghosh7331@gmail.com>
2024-02-06 12:45:45 +0530

    Feature: Custom title.

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,7 +1,13 @@
-import Scene from '../../components/3D_Error'
+import Scene from "../../components/3D_Error";
+import Head from "next/head";
 
 export default function Custom404() {
   return (
-    <Scene/>
-  )
+    <>
+      <Head>
+        <title>404 | Srijit.life</title>
+      </Head>
+      <Scene />
+    </>
+  );
 }
