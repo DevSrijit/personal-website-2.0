928d2b3f79a2583caaf4a8889fc86536f65a75ec
devsrijit <srijitghosh7331@gmail.com>
2024-02-06 12:36:15 +0530

    Feature: Amazing new 404 3D page.

---
+++ components/3D/index.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Scene(...props) {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Spline scene="https://prod.spline.design/pKEeM8GiT-IfUObf/scene.splinecode"/>
+    </div>
+  );
+}
